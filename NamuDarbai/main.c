a246e3c1fc7fc1a18f9b805454f016e0fd87b9d7
Artūras Kaziukonis <amdartur@gmail.com>
2018-04-24 22:53:28 +0300

    Padaryta 1 ir 2 uzduotys. Trecia padeta

--- NamuDarbai/main.c
+++ NamuDarbai/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 struct medis {
     int elementas;
@@ -11,31 +12,63 @@ struct medis {
 typedef struct medis Medis;
 typedef Medis *MedisPtr;
 
+void meniu();
 void Uzduotis_1 ();
 void Uzduotis_2 ();
 void Uzduotis_3 ();
 void Uzduotis_4 ();
 void iterpti (MedisPtr *, int);
-void spausdinti1(MedisPtr);
-void spausdinti2(MedisPtr);
-void spausdinti3(MedisPtr);
-void spausdinti4(MedisPtr);
+void spausdinti(MedisPtr);
+bool paieska(MedisPtr, int);
 
 int main()
 {
-    Uzduotis_2();
+	meniu();
+	system("pause");
     return 0;
+}
+
+void meniu() {
+	int i;
+	system("cls");
+	printf("\n=====================\n");
+	printf("  Uzduotys 2018\n");
+	printf("=====================\n\n");
+	printf("[1] - Uzduotis nr 1\n");
+	printf("[2] - Uzduotis nr 2\n");
+	printf("[3] - Uzduotis nr 3\n");
+	printf("[0] - Baigti darba\n");
+	scanf("%d", &i);
+
+	switch (i) {
+		case 0:
+			break;
+		case 1:
+			system("cls");
+			Uzduotis_1();
+			break;
+		case 2:
+			system("cls");
+			Uzduotis_2();
+			break;
+        case 3:
+            system("cls");
+			Uzduotis_3();
+			break;
+		default:
+			meniu();
+	}
 }
 
 void Uzduotis_1() {
     FILE * failas;
-    char tekstas[40], eilute[200];
-    int skaicius;
+    char tekstas[40];
+    int skaicius = 0;
 
     printf("UZDUOTIS 1\n");
-    printf("\nDuotas tipinis failas, kurio komponentai simboliai. Rasti kiek zodziu prasideda raide a.\n\n"); //Užduotis
+    printf("\nDuotas tipinis failas, kurio komponentai simboliai. Rasti kiek zodziu prasideda raide a.\n\n"); //UË›duotis
 
-    if ((failas = fopen("failas.dat", "wb")) == NULL){
+    if ((failas = fopen("failas.dat", "wb+")) == NULL){
         printf("Klaida atidarant faila!\n");
     } else {
         printf("Iveskite simbolius (teksta)\n");
@@ -43,38 +76,38 @@ void Uzduotis_1() {
         printf("?");
         scanf("%s", tekstas);
         while(!feof(stdin)) {
-            fprintf(failas, "%s ", tekstas);
+            fwrite(&tekstas, sizeof(tekstas), 1, failas);
             printf("?");
             scanf("%s", tekstas);
         }
-        fclose(failas);
     }
+	rewind(failas);
+	while(!feof(failas)){
+		fread(&tekstas, sizeof(tekstas), 1, failas);
+		char *zodis = strtok(tekstas, " ,");
+		if(zodis[0] == 'a') skaicius++;
+		zodis = strtok(NULL, " ,");
 
-    if ((failas = fopen("failas.dat", "rb")) == NULL) {
-        printf("Klaida atidarant faila!\n");
-    } else {
-        fgets(eilute, sizeof(eilute), failas);
-        char *zodis = strtok(eilute, " ,");
-        while(zodis != NULL) {
-            if(zodis[0] == 'a') skaicius++;
-            zodis = strtok(NULL, " ,");
-        }
-        fclose(failas);
-    }
+	}
+    fclose(failas);
 
     printf("\nYra %d zodziai su pirma a raide\n", skaicius);
+	system("pause");
+	meniu();
 }
 
 void Uzduotis_2() {
     FILE *failas;
-    double skaicius;
+    double skaicius, max = 0, suma = 0, sandauga = 1;
+    int i = 0, j;
 
     printf("UZDUOTIS 2\n\n");
     printf("Diske sukurkite tipizuota realiuju skaiciu faila. Skaiciu kiekis  nera zinomas.\n");
     printf("Po to, skaitydami duomenis is failo, apskaiciuokite suma elementu, esanciu nuo failo\n");
     printf("pradzios iki pirmo maksimalaus elemento (gali buti keli vienodi elementai, kuriu reiksme maksimali).\n");
     printf("Taip pat rasti sandauga elementu, esanciu nuo paskutinio maksimalaus elemento iki failo galo.\n\n");
-/*    if((failas = fopen("failas.dat", "wb")) == NULL) {
+
+    if((failas = fopen("failas.dat", "wb+")) == NULL) {
         printf("Klaida atidarant faila!");
     } else {
         printf("Iveskite realius skaicius\n");
@@ -82,43 +115,92 @@ void Uzduotis_2() {
         printf("?");
         scanf("%lf", &skaicius);
         while(!feof(stdin)) {
-            fprintf(failas, "%.2f ", skaicius);
+            fwrite(&skaicius, sizeof(double), 1, failas);
             printf("?");
             scanf("%lf", &skaicius);
+            i++;
         }
+		rewind(failas);
+		printf("Elementai: ");
+        for (j=0;j<i;j++){
+            fread(&skaicius, sizeof(double), 1, failas);
+			if(skaicius > max) {
+				max = skaicius;
+			}
+            printf("%.2f ", skaicius);
+        }
+		rewind(failas);
+		skaicius = 0;
+		while (skaicius != max) {
+			suma = suma + skaicius;
+			fread(&skaicius, sizeof(double), 1, failas);
+			i--;
+		}
+		for (j=0; j<i; j++) {
+			fread(&skaicius, sizeof(double), 1, failas);
+			sandauga = sandauga * skaicius;
+			if (skaicius == max) sandauga = 1;
+		}
+		if (max == 0) sandauga = 0;
+
+		printf("\nMaksimalus skaicius yra: %.2lf ", max);
+		printf("\nSuma yra (iki pirmo maksimalaus): %.2lf", suma);
+		printf("\nSandauga yra (nuo paskutinio maksimalaus): %2.lf\n", sandauga);
         fclose(failas);
     }
-*/
-    if ((failas = fopen("failas.dat", "rb")) == NULL) {
-        printf("Klaida atidarant faila!\n");
-    } else {
-        while(!feof(failas)){
-            fscanf(failas, "%lf", &skaicius);
-            printf("%.2f\n", skaicius);
-        }
-    }
+	system("pause");
+	meniu();
 }
 
 void Uzduotis_3() {
     int i, reiksme;
     MedisPtr saknis = NULL;
-    printf("Sukurti dvejetaini medi, realizuojant elemento iterpimo, pasalinimo, spausdinimo bei paieskos funkcijas.\n");
+	
+	system("cls");
+    printf("Uzduotis 3\n\n");
+    printf("Sukurti dvejetaini medi, realizuojant elemento iterpimo, pasalinimo, \nspausdinimo bei paieskos funkcijas.");
     printf("Perkelti to saraso visus elementus i dvikrypti ciklini sarasa. \nAtspausdinti visus dvikrypcio ciklinio saraso elementus.\n\n");
-    printf("Iveskite 6 reiksmiu i medi\n");
-    for (i=0; i<=5; i++) {
-        scanf("%d", &reiksme);
-        printf("Kita: \n");
-        iterpti(&saknis, reiksme);
-    }
+    //Meniu
+	printf("[1] - Iterpti\n");
+	printf("[2] - Pasalinti\n");
+	printf("[3] - Spauzdinti\n");
+	printf("[4] - Ieskoti\n");
+	printf("[5] - Perkelti visus elementus i dvikrypti ciklini sarasa ir atspauzdinti\n");
+	printf("[0] - Gryzti atghal\n");
+	scanf("%d", &i);
 
-    printf("Budas nr 1\n");
-    spausdinti1(saknis);
-    printf("\nBudas nr 2\n");
-    spausdinti2(saknis);
-    printf("\nBudas nr 3\n");
-    spausdinti3(saknis);
-    printf("\nBudas nr 4\n");
-    spausdinti4(saknis);
+	switch (i) {
+		case 0:
+			meniu();
+			exit(0);
+			break;
+		case 1:
+			printf("Iveskite 5 reiksmiu i medi\n");
+			for (i=0; i<5; i++) {
+				scanf("%d", &reiksme);
+				printf("Kita: \n");
+				iterpti(&saknis, reiksme);
+			}
+			break;
+		case 2:
+			break;
+		case 3:
+		    printf("\nMedis:\n");
+			spausdinti(saknis);
+			break;
+		case 4:
+			printf("Ko ieskoti?\n?");
+			scanf("%d", &reiksme);
+			paieska(saknis, reiksme);
+			break;
+		case 5:
+			break;
+		default:
+			printf("Neteisingas pasirinkimas!");
+			break;
+	}
+    system("pause");
+	meniu();
 }
 
 void iterpti(MedisPtr *MusuMedis, int elem) {
@@ -136,35 +218,23 @@ void iterpti(MedisPtr *MusuMedis, int elem) {
     }
 }
 
-void spausdinti1(MedisPtr MusuMedis) {
+void spausdinti(MedisPtr MusuMedis) {
     if (MusuMedis != NULL) {
-        spausdinti1(MusuMedis->kaire);
+        spausdinti(MusuMedis->kaire);
         printf("%3d", MusuMedis->elementas);
-        spausdinti1(MusuMedis->desine);
+        spausdinti(MusuMedis->desine);
     }
 }
 
-void spausdinti2(MedisPtr MusuMedis) {
-    if(MusuMedis != NULL){
-        printf("%3d", MusuMedis->elementas);
-        spausdinti2(MusuMedis->kaire);
-        spausdinti2(MusuMedis->desine);
-    }
+bool paieska(MedisPtr MusuMedis, int elem) {
+	while(MusuMedis != NULL) {
+		if((MusuMedis)->elementas == elem) {
+			return true;
+		} else if(elem < (MusuMedis)->elementas) {
+			MusuMedis = (MusuMedis)->kaire;
+		} else {
+			MusuMedis = (MusuMedis)->desine;
+		}
+	}
+	return false;
 }
-
-void spausdinti3(MedisPtr MusuMedis) {
-    if(MusuMedis != NULL){
-        spausdinti3(MusuMedis->kaire);
-        spausdinti3(MusuMedis->desine);
-        printf("%3d", MusuMedis->elementas);
-    }
-}
-
-void spausdinti4(MedisPtr MusuMedis) {
-    if(MusuMedis != NULL) {
-        spausdinti4(MusuMedis->desine);
-        printf("%3d", MusuMedis->elementas);
-        spausdinti4(MusuMedis->kaire);
-    }
-}
-
