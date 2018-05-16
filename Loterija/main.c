026b1883e3a5570397dc5534e7ea823523198e73
Artūras Kaziukonis <amdartur@gmail.com>
2018-05-16 23:12:22 +0300

    Numerio generavimas

--- Loterija/main.c
+++ Loterija/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	FILE * failas;
+	int Numeris_1, i;
+	
+	if((failas = fopen("BILIETAI.txt", "w")) == NULL) {
+		printf("Klaida atidarant failą BILIETAI.txt\n");
+	} else {
+		srand(time(NULL));
+		for(i=0; i<9999; i++) {
+			Numeris_1 = rand() % 9999;
+			fprintf(failas, "%04d\n", Numeris_1);
+		}
+	}
+	fclose(failas);
+	system("pause");
 	return 0;
 }
